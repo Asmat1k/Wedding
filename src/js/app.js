6e839831c344250f2bb392bc1fef2a972e605857
Asmat1k <dobrovti@mail.ru>
2023-05-16 13:01:54 +0300

    feat: new google-form done

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 import { closeLook } from "./functions/close-look.js";
 import { burger } from "./functions/burger.js";
+//  import { form } from "./functions/form.js";
 
 closeLook();
 burger();
+// form();
 
 flsFunctions.isWebp();
